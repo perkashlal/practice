593e929fa38e69d938af2f564b4117c68e5e9d18
stech <stech@samtani>
2025-04-14 18:54:53 +0200

    my first commit of first project

---
+++ FirstJavaProject/src/BasicPrintClass.java
@@ -0,0 +1,15 @@
+
+public class BasicPrintClass {
+
+	public static void main(String[] arg) {
+		
+		System.out.println("hey Parkash");
+		
+		int a = 5;
+		int b = 3;
+		int c=a+b;
+		System.out.println("sum of a + b : "+ c);
+		
+	}
+	
+}
